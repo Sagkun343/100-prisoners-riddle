a1a69061e3600be02d4c101c0ea1dac1c4f1d5b6
Sagkun343 <135689643+Sagkun343@users.noreply.github.com>
2023-09-22 02:47:56 +0530

    Update prisoners dilemma wo feedback.py

--- prisoners dilemma wo feedback.py
+++ prisoners dilemma wo feedback.py
@@ -1,35 +1,49 @@
-#Prisoners dilemma by veritasium
-import random
-import time
-start = time.time()
-op_list = []
-for i in range(100000):
-    rand_set = set()
-    number_list = []
-    while len(number_list) != 100:
-        number = random.randint(1,100)
-        if number not in rand_set:
-            number_list.append(number)
-            rand_set.add(number)
-    number_list = tuple(number_list)
-    for prisoner in range(1, 101):
-        iter = 50
-        index = prisoner
-        flag = True
-        while iter != 0:
-            val = number_list[index - 1]
-            index = val
-            if index == prisoner:
-                break
-            else:
-                iter -= 1
-            if iter == 0:
-                flag = False
-        if flag is False:
-            op_list.append(0)
-            break
-    if flag is True:
-        op_list.append(1)
-print(op_list.count(1)) # average success 31.26%
-end= time.time()
-print(end - start)
+import random                                                               
+import time                                                                 
+def prisoners_dilemma(iteration_count: int,chances: int, prisoner_number: in
+    start = time.time()                                                     
+    op_list = []                                                            
+    for i in range(iteration_count):                                        
+        rand_set = set()                                                    
+        number_list = []                                                    
+        while len(number_list) != prisoner_number:#assigns a random integer 
+            number = random.randint(1, prisoner_number)                     
+            if number not in rand_set:                                      
+                number_list.append(number)                                  
+                rand_set.add(number)                                        
+        number_list = tuple(number_list)                                    
+        for prisoner in range(1, prisoner_number + 1):# iterates and checks 
+            iter = chances                                                  
+            index = prisoner                                                
+            flag = True                                                     
+            while iter != 0:                                                
+                number_set = set()                                          
+                val = number_list[index - 1]                                
+                index = val                                                 
+                if val in number_set:                                       
+                    while True: #edge-case where the prisoner knows they're 
+                        rand = random.randint(1, prisoner_number)           
+                        if rand not in number_set:                          
+                            index = rand                                    
+                            break                                           
+                else:                                                       
+                    number_set.add(val)                                     
+                if index == prisoner:                                       
+                    break                                                   
+                else:                                                       
+                    iter -= 1                                               
+                if iter == 0:                                               
+                    flag = False                                            
+            if flag is False:                                               
+                op_list.append(0)                                           
+                break                                                       
+        if flag is True:                                                    
+            op_list.append(1)                                               
+    success_percentage = (op_list.count(1) / iteration_count) * 100         
+    print(f"success percentage = {success_percentage}%")# increased average 
+    end= time.time()                                                        
+    print(f"time taken:{end - start}")                                      
+    return None                                                             
+                                                                            
+if __name__ == "__main__":                                                  
+    prisoners_dilemma(100000, 50, 100)                                      
